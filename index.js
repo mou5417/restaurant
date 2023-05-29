0bccef87238eef8409d87bd5d756bcb2078bb5a2
Mou <mouiscoding@gmail.com>
2023-05-29 15:02:50 +0800

    add mobile ui

--- index.js
+++ index.js
@@ -42,8 +42,8 @@ function render() {
     let id=item.id
     itemtext += ` <div class="item-outer"> 
 <div class="item"> 
- <img class="item-img" src="./img/${nametolower}.png" alt="">
- <div class="item-detail">
+  <img class="item-img" src="./img/${nametolower}.png" alt="">
+  <div class="item-detail">
     <div class="item-name"id="item-name">
         ${item.name}
     </div>
